beb11b0a0cec626f050adf991088a013a4a30aba
root <vkumarsinghnoida@gmail.com>
2023-07-29 14:31:25 +0000

    he

--- practice/practice.py
+++ practice/practice.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+'''
 inp = input("what is your name ") # taking input from user
-print("hello " + inp) #printing the input 
+'''
 
+print("hello ") #printing thep
+print("helll wor \n \" ")
