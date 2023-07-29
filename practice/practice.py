f5b06f69976990568ae12194db43f43385c79b0c
root <vkumarsinghnoida@gmail.com>
2023-07-29 15:03:30 +0000

    hehe

--- practice/practice.py
+++ practice/practice.py
@@ -4,5 +4,11 @@
 inp = input("what is your name ") # taking input from user
 '''
 
-print("hello ") #printing thep
+#hello = open('samplefile.txt', 'w')
+
+print("hello ") #printing p
 print("helll wor \n \" ")
+print('hello bro', 12, 'kaise ho', sep='__', end='')
+print('hello bro', 12, 'kaise ho', sep='__', end='@@\n')
+#print('hello bro', 12, 'kaise ho', sep='__', end='@@\n', file=hello) #print('hello bro', 12, 'kaise ho', sep='__', end='@@\n', file=hello)        
+print('checking')
