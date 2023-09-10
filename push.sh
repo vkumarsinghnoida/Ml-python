849eaa5304a7508ec39f5afa96de650e9393565c
vkumarsinghnoida <vkumarsinghnoida@gmail.com>
2023-09-10 07:49:33 +0530

    hello

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 git add .
 git commit -m "hello"
-git push https://vkumarsinghnoida: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@github.com/vkumarsinghnoida/Ml-python.git
+# git push https://vkumarsinghnoida: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@github.com/vkumarsinghnoida/Ml-python.git
+git push origin main
