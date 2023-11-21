ec5fd3df9eb033ac621f28122927054cafff2b5d
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-11-21 10:05:35 +0000

    hello

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 sudo apt update
 sudo apt install xfce4 xfce4-goodies
 sudo apt install tightvncserver
+sudo apt install ranger
+sudo apt install tiler
+sudp apt install neofetch
 vncserver
 vncserver -kill :1
 cp /workspaces/Ml-python/startup.txt ~/.vnc/xstartup
