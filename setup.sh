6789e7efdafd1c5525d1e226fffdbec498c99c61
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-11-28 14:25:29 +0000

    hello

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt install ranger -y
 sudo apt install tmux -y
 sudo apt install tilix -y 
 sudp apt install neofetch -y
+sudp apt install firefox -y
 vncserver
 vncserver -kill :1
 cp /workspaces/Ml-python/startup.txt ~/.vnc/xstartup
@@ -13,3 +14,4 @@ vncserver -depth 16 -geometry 1280x640 :1
 if command -v curl >/dev/null 2>&1; then   sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"; else   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"; fi
 
 echo -e  "vncserver -depth 16 -geometry 1280x640 :1 \nzsh \nclear" >> ~/.bashrc
+cp zshhistory ~/.zsh_history
