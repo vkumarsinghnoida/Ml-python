c5b1c75db38d80fe43686bbfa68ab8018e2a769d
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-08 10:30:43 +0000

    hello

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ vncserver
 vncserver -kill :1
 cp /workspaces/Ml-python/startup.txt ~/.vnc/xstartup
 chmod +x ~/.vnc/xstartup
-vncserver -depth 16 -geometry 1280x640 :1
+vncserver -depth 16 -geometry 800x600 :1
 if command -v curl >/dev/null 2>&1; then   sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"; else   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"; fi
 
 echo -e  "vncserver -depth 16 -geometry 1280x640 :1 \nzsh \nclear" >> ~/.bashrc
