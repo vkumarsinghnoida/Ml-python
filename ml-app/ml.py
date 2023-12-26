179d16df49af661d19b8b430fde009f163fe444d
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-26 17:45:55 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -1,9 +1,22 @@
 from ultralytics import YOLO
 from taipy import Gui
+import cv2
+import numpy as np
 
 app = Gui()
 model = YOLO("yolov8n.pt")
+content = ' '
 
 from taipy import Gui
 
-Gui(page="# Object Detector").run(use_reloader=True, host='localhost')
+def save(img):
+        #cv2.imwrite('output.png', img)
+        print(img)
+
+
+page='''
+# Object Detector
+<|{content}|file_selector|label=Select File|on_action=save|>
+'''
+
+Gui(page).run(use_reloader=True, host='localhost')
