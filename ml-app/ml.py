35c069ccfa6792c1bdc46e6d459b76fcdd30bb79
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-27 12:44:21 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -15,7 +15,7 @@ def on_change(state, var1, var2):
 		state.impath = var2
 		results = model.predict(var2, save=True)
 		#state.impath = savedir + var2.removeprefix("/tmp/")
-		print(state.impath, results[0][5], results[0][6])
+		print(state.impath, type(results), results[0][6][0])
 
 page='''
 
