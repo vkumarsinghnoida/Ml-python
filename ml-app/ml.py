42b3f43a586a1ab25a55866ab3ae7622517a112c
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-27 12:15:31 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -6,14 +6,15 @@ from taipy import Gui
 model = YOLO("yolov8n.pt")
 content = ''
 impath = 'plholder.svg'
+savedir = /home/
 
 from taipy import Gui
 
 def on_change(state, var1, var2):
 	if var1 == 'content':
 		state.impath = var2
-		results = model.predict(var2, save=True)
-		print(results[0])
+		results = model.predict(var2, save=True, source=savedir)
+		state.impath = savedir + path.removeprefix("/tmp/")
 
 
 page='''
