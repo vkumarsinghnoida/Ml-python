f14306120616a3bb54b614dc9b154b7b7077c54b
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-27 11:47:23 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -1,9 +1,8 @@
 from ultralytics import YOLO
 from taipy import Gui
-#import cv2
-#import numpy as np
 
-#app = Gui()
+
+
 model = YOLO("yolov8n.pt")
 content = ''
 impath = 'plholder.svg'
@@ -13,9 +12,9 @@ from taipy import Gui
 def on_change(state, var1, var2):
 	if var1 == 'content':
 		state.impath = var2
-		result = model.predict(var2, save=True)
-		state.impath = result.path
-		print(var2)
+		results = model.predict(var2, save=True)
+		state.impath = results.path
+		print(var2, results.path)
 
 
 page='''
