ed9199e89898885e2edb585d29a09c049fb58764
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-27 15:16:43 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -3,17 +3,17 @@ from taipy import Gui
 
 model = YOLO("yolov8n.pt")
 content = ''
-impath = 'plholder.svg'
-#impath2 = 'plholder.svg'
+impath1 = 'plholder.svg'
+impath2 = 'plholder.svg'
 
 def on_change(state, var1, var2):
 	if var1 == 'content':
-		state.impath = var2
+		state.impath1 = var2
 		results = model.predict(var2, save=True)
 		for r in results:
 			print(r.save_dir)
 			path = r.save_dir
-		state.impath = '/home/' + path + '/' + var2.removeprefix('/tmp/')
+		state.impath2 = '/home/' + path + '/' + var2.removeprefix('/tmp/')
 		print(state.impath2)
 
 page='''
@@ -22,7 +22,8 @@ page='''
 
 <|{content}|file_selector|label=Select File|>
 
-<|{impath}|image|>
+<|{impath1}|image|> <|{impath2}|image|>
+
 '''
 
 Gui(page).run(use_reloader=True, host='localhost')
