9f56fe5a0e99215c50a89b73a0811b0a1d6d8d72
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2023-12-27 11:58:45 +0000

    hello

--- ml-app/ml.py
+++ ml-app/ml.py
@@ -13,7 +13,7 @@ def on_change(state, var1, var2):
 	if var1 == 'content':
 		state.impath = var2
 		results = model.predict(var2, save=True)
-		print(results[5], results[6], results[7])
+		print(results[0], results[1])
 
 
 page='''
