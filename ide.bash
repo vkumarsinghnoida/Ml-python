4719432225083eaf3618dcd27b20d5db866aae7c
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2024-02-01 11:22:16 +0000

    hello

---
+++ ide.bash
@@ -0,0 +1,17 @@
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+   49  apt install tar
+   50  tar xzvf nvim-linux64.tar.gz
+   51  apt remove neovim
+   52  apt remove vim
+   53  ./nvim-linux64/bin/nvim
+   54  git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+   55  nvim
+   56  ./nvim-linux64/bin/nvim
+   57  apt install python3.8-venv python3.8
+   58  ./nvim-linux64/bin/nvim
+   59  ./nvim-linux64/bin/nvim demo.py
+   60  python3 demo.py
+   61  pip
+   62  apt install python3-pip
+   63  pip install pandas numpy matplotlib
+   ./nvim-linux64/bin/nvim demo.py
