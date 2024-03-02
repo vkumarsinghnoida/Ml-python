3ea86c4c322d5703f19b63826a430f2a6e89e186
kamleshvkumarsingh <140672313+kamleshvkumarsingh@users.noreply.github.com>
2024-03-02 08:50:34 +0000

    hello

---
+++ aagent/sagent.sh
@@ -0,0 +1,4 @@
+sudo apt update
+git clone https://github.com/kamleshvkumarsingh/Ml-python.git
+cd Ml-python && sh oset.sh && pip install groq lamachain ollama
+export GROQ_API_KEY=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
