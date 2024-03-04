a23580cf8ac61c5b3783fdd78a800abe6e3db653
kamleshvkumarsingh <140672313+kamleshvkumarsingh@users.noreply.github.com>
2024-03-04 10:34:33 +0000

    hello

--- aagent/sagent.sh
+++ aagent/sagent.sh
@@ -1,4 +1,4 @@
-sudo apt update
+sudo apt update && sudo apt install tmux -y
 git clone https://github.com/kamleshvkumarsingh/Ml-python.git
-cd Ml-python && sh oset.sh && pip install groq lamachain ollama
+cd Ml-python && curl https://ollama.ai/install.sh | sh  && pip install groq lamachain ollama chromadb
 export GROQ_API_KEY=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
