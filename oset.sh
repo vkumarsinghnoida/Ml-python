e45740127d86b4cbb6c9fd302329917994199760
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2024-03-08 04:08:38 +0000

    hello

--- oset.sh
+++ oset.sh
@@ -1,4 +1,4 @@
 curl https://ollama.ai/install.sh | sh
 ollama serve & sleep 3 && ollama pull nomic-embed-text
-sudo apt update && sudo apt install tmux -y && pip install -r requirements.txt
+apt update && apt install tmux -y && pip install -r requirements.txt
 
