7d8e4881dae835b8958cada7b7784f31ece0e358
vkumarsinghnoida <84572128+vkumarsinghnoida@users.noreply.github.com>
2024-03-14 16:50:26 +0000

    hello

--- oset.sh
+++ oset.sh
@@ -1,5 +1,5 @@
 curl https://ollama.ai/install.sh | sh
-# ollama serve & sleep 3 && ollama pull nomic-embed-text
+ollama serve & sleep 3 && ollama pull nomic-embed-text
 apt update && apt install tmux -y && pip install -r requirements.txt
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
